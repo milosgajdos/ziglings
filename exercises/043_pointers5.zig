3ed0f140d1b6052cbb2276f2435804392b8bbf1f
Milos Gajdos <milosthegajdos@gmail.com>
2024-10-06 21:38:15 +0100

    Solutions to 39-49
    Signed-off-by: Milos Gajdos <milosthegajdos@gmail.com>

--- exercises/043_pointers5.zig
+++ exercises/043_pointers5.zig
@@ -54,7 +54,7 @@ const Character = struct {
 
 pub fn main() void {
     var mighty_krodor = Character{
-        .class = Class.wizard,
+        .class = .wizard,
         .gold = 10000,
         .experience = 2340,
     };
@@ -68,7 +68,7 @@ pub fn main() void {
 
     // FIX ME!
     // Please pass Glorp to printCharacter():
-    printCharacter(???);
+    printCharacter(&glorp);
 }
 
 // Note how this function's "c" parameter is a pointer to a Character struct.
